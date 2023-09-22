33f8238cdb6c68e79c6c8a4de7c9ba07a8da1b78
User <User@DESKTOP-CRJD420>
2023-09-21 23:09:12 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio1For/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Ingrese el número 1: ");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el número 2: ");
+int numero2 = int.Parse(Console.ReadLine());
+
+int resultado = Multiplicar(numero1, numero2);
+
+Console.WriteLine("El producto es:" +resultado+ "");
+
+int Multiplicar(int a, int b)
+{
+    int resultado = 0;
+
+    if (a < 0 && b < 0)
+    {
+        a = -a;
+        b = -b;
+    }
+
+    for (int i = 0; i < Math.Abs(b); i++)
+    {
+        resultado = resultado + a;
+    }
+
+    return resultado;
+}
